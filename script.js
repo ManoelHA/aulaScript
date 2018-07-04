c2560f652e88d288cfa9e31af8a4b327f5a05822
Henrique Martins <alvesmartins5@hotmail.com>
2018-07-03 21:54:44 -0300

    Aula 1 parte 1 adicinado

---
+++ script.js
@@ -0,0 +1,20 @@
+function apertouMouse(){
+
+console.log("Apertou o Mouse!");
+}
+
+function soltouMouse(){
+	console.log("Soltou o Mouse!");
+}
+
+function mouseCima(){
+	console.log("Mouse em Cima");
+}
+
+function mouseSaiu(){
+	console.log("Mouse saiu de cima");
+}
+
+function mouseMovendo(){
+	console.log("Mouse Movendo!")
+}
