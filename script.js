db57d10063b7a80fd6355235da0efc435f08bce2
Henrique Martins <alvesmartins5@hotmail.com>
2018-07-03 22:04:55 -0300

    aula 1.2

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-function apertouMouse(){
-
-console.log("Apertou o Mouse!");
-}
-
-function soltouMouse(){
-	console.log("Soltou o Mouse!");
-}
-
-function mouseCima(){
-	console.log("Mouse em Cima");
-}
-
-function mouseSaiu(){
-	console.log("Mouse saiu de cima");
-}
-
-function mouseMovendo(){
-	console.log("Mouse Movendo!")
+function cliqueMouse(){
+	console.log("Clicou no Mouse");
 }
